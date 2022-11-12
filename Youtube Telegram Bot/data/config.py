da7fc975c6ea22b6d3846b84b2a1f08e77eaa086
BTBRuslan <115881061+BTBRuslan@users.noreply.github.com>
2022-11-12 17:53:25 +0400

    Update config.py

--- Youtube Telegram Bot/data/config.py
+++ Youtube Telegram Bot/data/config.py
@@ -5,6 +5,6 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+BOT_TOKEN = env.str("5728997840:AAGk_cTjeD-tQCuACMpcBlaNLMS4bZLwA6s")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 IP = env.str("ip")  # Xosting ip manzili
